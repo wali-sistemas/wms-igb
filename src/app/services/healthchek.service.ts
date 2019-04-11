41d806013b332fa6a9f69d0a302193fac4c013a0
jguisaor <jguisaorestrepo@gmail.com>
2019-04-11 09:03:06 -0500

    Subiendo cambios para mejoras

---
+++ src/app/services/healthchek.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { GLOBAL } from './global';
+
+@Injectable()
+export class Healthchek {
+  public urlManager: string;
+
+  constructor(private _http: Http) {
+    this.urlManager = GLOBAL.urlManager;
+  }
+
+  public resetSessionId() {
+    return this._http.get(this.urlManager + 'health/reset')
+      .map(res => res.json());
+  }
+}
