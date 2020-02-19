015ea562adb6171bdb611f32df1fcc9748cef71e
wali <46688722+wali-sistemas@users.noreply.github.com>
2020-02-19 14:55:02 -0500

    Revert "Ajustes febrero 2020 (#63)"
    This reverts commit 5f9d19b7b37017bcac6b3e0326948fc77ee21f03.

--- src/app/components/picking/picking.component.ts
+++ src/app/components/picking/picking.component.ts
@@ -48,8 +48,7 @@ export class PickingComponent implements OnInit {
     public availableCarts: Array<BinLocation>;
     public assignedOrders: Array<SalesOrder>;
     public pickingItems: Array<any>;
-    public position: number = 1;
-    public countLineNum: number = 0;
+    public position: number = 0;
 
     constructor(private _userService: UserService,
         private _salesOrderService: SalesOrdersService,
@@ -161,40 +160,19 @@ export class PickingComponent implements OnInit {
             keyboard: false,
             show: true
         });
-
-        /*let ordenes = "";
-        if (this.selectedOrder == "") {
-            for (let i = 0; i < this.assignedOrders.length; i++) {
-                ordenes += this.assignedOrders[i].docNum + ",";
-            }
-            ordenes = ordenes.substring(0, ordenes.length - 1);
-        } else { ordenes = this.selectedOrder; }*/
-
-        this._pickingService.getNextPickingItem(this.identity.username, this.selectedOrder == "" ? null : this.selectedOrder, this.position).subscribe(
+        this._pickingService.getNextPickingItem(this.identity.username, this.selectedOrder).subscribe(
             response => {
                 console.log("Lista de ítem para picking ", response);
                 if (response.code === 0) {
                     this.pickingItems = response.content;
-
-                    /*this.nextItemCode = response.content[this.position].itemCode.trim();
+                    this.nextItemCode = response.content[this.position].itemCode.trim();
                     this.nextItemQuantity = response.content[this.position].pendingQuantity;
                     this.nextBinAbs = response.content[this.position].binAbs;
                     this.nextBinStock = response.content[this.position].availableQuantity;
                     this.nextBinLocationCode = response.content[this.position].binCode;
                     this.nextItemName = response.content[this.position].itemName;
                     this.nextOrderNumber = response.content[this.position].orderNumber;
-                    this.nextBinType = response.content[this.position].binLocationType;*/
-
-                    this.nextItemCode = response.content.itemCode.trim();
-                    this.nextItemQuantity = response.content.pendingQuantity;
-                    this.nextBinAbs = response.content.binAbs;
-                    this.nextBinStock = response.content.availableQuantity;
-                    this.nextBinLocationCode = response.content.binCode;
-                    this.nextItemName = response.content.itemName;
-                    this.nextOrderNumber = response.content.orderNumber;
-                    this.nextBinType = response.content.binLocationType;
-                    this.countLineNum = response.content.lineNum;
-
+                    this.nextBinType = response.content[this.position].binLocationType;
                     $('#modal_loading_next').modal('hide');
                     $('#binLoc').focus();
                 } else if (response.code === -1) {
@@ -212,7 +190,6 @@ export class PickingComponent implements OnInit {
             }, error => {
                 $('#modal_loading_next').modal('hide');
                 console.error(error);
-                this.redirectIfSessionInvalid(error);
                 this.errorMessage = 'Ocurrió un error al consultar el siguiente ítem para picking. ';
             }
         );
@@ -342,7 +319,7 @@ export class PickingComponent implements OnInit {
         document.getElementById("loc").style.display = "none";
 
         //reload next item
-        this.position = 1;
+        this.position = 0;
         if (this.selectedCart <= 0) {
             this.loadNextItem();
         }
@@ -441,20 +418,16 @@ export class PickingComponent implements OnInit {
     public getBackItem() {
         if (this.position > 0) {
             this.position--;
-        } else if (this.position <= 0) {
-            this.position = 1;
+            this.loadNextItem();
         }
-        console.log("Posición " + this.position + " de " + this.countLineNum + " para picking");
-        this.loadNextItem();
     }
 
     public getNextItem() {
-        if (this.position <= this.countLineNum - 1) {
+        if (this.position < this.pickingItems.length - 1) {
             this.position++;
         } else {
-            this.position = 1;
+            this.position = 0;
         }
-        console.log("Posición " + this.position + " de " + this.countLineNum + " para picking");
         this.loadNextItem();
     }
 
