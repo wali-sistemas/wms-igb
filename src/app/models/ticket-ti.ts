ff2e2981a6ed62c384784f9ed1798fb7d0197c25
wali <46688722+wali-sistemas@users.noreply.github.com>
2020-08-12 06:27:16 -0500

    Dashboard pedido (#77)
    * Subiendo desarrollo
    
    * Subiendo cambios en Ticket
    
    Co-authored-by: wali <sistemas2@igbcolombia.com>

---
+++ src/app/models/ticket-ti.ts
@@ -0,0 +1,24 @@
+export class TicketTI {
+    public idTicket: Number;
+    public idTypeTicket: Number;
+    public asunt: String;
+    public typeTicket: String;
+    public dateTicket: Date;
+    public department: String;
+    public empAdd: String;
+    public empSet: String
+    public urlAttached: String;
+    public priority: String;
+    public company: String;
+    public status: String;
+    public notes: Array<TicketTINotes>;
+    constructor() { }
+}
+
+export class TicketTINotes {
+    public idTicket: Number;
+    public dateNote: Date;
+    public empNote: String;
+    public note: String;
+    constructor() { }
+}
--- src/app/services/ticket-TI.service.ts
+++ src/app/services/ticket-TI.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { GLOBAL, IGBHeaders } from './global';
+import { TicketTINotes, TicketTI } from 'app/models/ticket-ti';
 
 @Injectable()
 export class TicketTIService {
@@ -10,8 +11,38 @@ export class TicketTIService {
         this.urlManager = GLOBAL.urlManager;
     }
 
-    public ticketList() {
-        return this._http.get(this.urlManager + 'ticket/list')
+    public listTicket(empId: string) {
+        return this._http.get(this.urlManager + 'ticket/list/' + empId)
+            .map(res => res.json());
+    }
+
+    public listNotesTicket(idTicket: Number) {
+        return this._http.get(this.urlManager + 'ticket/notes/' + idTicket)
+            .map(res => res.json());
+    }
+
+    public listTypeTickets() {
+        return this._http.get(this.urlManager + 'ticket/type-tickets')
+            .map(res => res.json());
+    }
+
+    public addNoteTicket(ticketNotesDTO: TicketTINotes) {
+        return this._http.post(this.urlManager + 'ticket/add-note', ticketNotesDTO)
+            .map(res => res.json());
+    }
+
+    public addNewTicket(ticketDTO: TicketTI) {
+        return this._http.post(this.urlManager + 'ticket/add-new-ticket', ticketDTO)
+            .map(res => res.json());
+    }
+
+    public assignedTicket(ticketDTO: TicketTI, username: String) {
+        return this._http.post(this.urlManager + 'ticket/assign-ticket?username=' + username, ticketDTO)
+            .map(res => res.json());
+    }
+
+    public changeStatusTicket(ticketDTO: TicketTI) {
+        return this._http.post(this.urlManager + 'ticket/update-status-ticket', ticketDTO)
             .map(res => res.json());
     }
 }
