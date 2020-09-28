68d13c5dc2065a986e2cf7cc125d1f62af53c7ae
wali <46688722+wali-sistemas@users.noreply.github.com>
2020-09-28 13:39:52 -0500

    Subiendo cambios (#87)
    Co-authored-by: wali <sistemas2@igbcolombia.com>

--- src/app/components/report/manager/report.manager.component.ts
+++ src/app/components/report/manager/report.manager.component.ts
@@ -312,7 +312,9 @@ export class ReportManagerComponent implements OnInit {
         this.ordersOfDay = new Array<any>();
         this._reportService.listOrdersOfDay(this.queryParam.id, false).subscribe(
             response => {
+                console.log(response);
                 this.ordersOfDay = response;
+                console.log(this.ordersOfDay);
                 this.barChartDataOrderOfDay = [this.ordersOfDay[0][1], this.ordersOfDay[1][1], this.ordersOfDay[2][1], this.ordersOfDay[3][1], this.ordersOfDay[4][1]];
                 $('#modal_transfer_process').modal('hide');
             }, error => {
