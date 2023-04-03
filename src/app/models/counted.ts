822433827359b9f94778f14bd54a4437a7ae0bca
wali <46688722+wali-sistemas@users.noreply.github.com>
2023-04-03 16:18:51 -0500

    Front (#197)
    * Subir cambio a master
    
    * Subir cambios de shipping
    
    * Subir cambios
    
    * Subir cambios comex
    
    * Subir cambios
    
    * Subir cambios a Comex
    
    * Subir cambios a master
    
    * Subir cambios
    
    * Subir cambio
    
    * subir cambio
    
    * Subir cambios KPI
    
    * Subir cambios a mtz
    
    * Subir cambios a traslados
    
    * Subiendo cambios
    
    * Subir cambios a shipping
    
    * Subiendo cambios
    
    * Subir cambios
    
    * Subir cambios del 2023
    
    * Subir cambios de redplas
    
    * Subir cambios
    
    * Subir cambios a master
    
    * Subir cambios
    
    * Subir cambios a shipping
    
    * Subir cambios a OLA
    
    * Subir cambios OLA
    
    * Subir cambios
    
    * Subir cambios a ticket
    
    * Subir cambios
    
    * subir cambios
    
    * Subir cambios modulo cartera

---
+++ src/app/models/counted.ts
@@ -0,0 +1,13 @@
+export class Counted {
+  public balance: number;
+  public cardCode: string;
+  public cardName: string;
+  public category: string;
+  public day: number;
+  public docDate: Date;
+  public docNum: number;
+  public docTotal: number;
+  public slpName: string;
+
+  constructor() { }
+}
--- src/app/services/invoice.service.ts
+++ src/app/services/invoice.service.ts
@@ -4,15 +4,27 @@ import { GLOBAL, IGBHeaders } from './global';
 
 @Injectable()
 export class InvoiceService {
-    public url: string;
+  public url: string;
 
-    constructor(private _http: Http) {
-        this.url = GLOBAL.url;
-    }
+  constructor(private _http: Http) {
+    this.url = GLOBAL.url;
+  }
 
-    public createInvoice(deliveryDocEntry) {
-        let igbHeaders = new IGBHeaders().loadHeaders();
-        return this._http.post(this.url + 'invoice', deliveryDocEntry, { headers: igbHeaders })
-            .map(res => res.json());
-    }
+  public createInvoice(deliveryDocEntry) {
+    let igbHeaders = new IGBHeaders().loadHeaders();
+    return this._http.post(this.url + 'invoice', deliveryDocEntry, { headers: igbHeaders })
+      .map(res => res.json());
+  }
+
+  public listCashInvoices() {
+    let igbHeaders = new IGBHeaders().loadHeaders();
+    return this._http.get(this.url + 'invoice/cash-invoices', { headers: igbHeaders })
+      .map(res => res.json());
+  }
+
+  public updateStatusCashInvoice(docNum: number, status: string) {
+    let igbHeaders = new IGBHeaders().loadHeaders();
+    return this._http.put(this.url + 'invoice/cash-invoice/update-status/?docnum=' + docNum + '&status=' + status, null, { headers: igbHeaders })
+      .map(res => res.json());
+  }
 }
