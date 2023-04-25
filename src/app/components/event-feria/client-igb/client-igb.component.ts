0480f7fb84ca61c7b9ef8c042f96340cd60bc630
wali <46688722+wali-sistemas@users.noreply.github.com>
2023-04-25 14:11:58 -0500

    Front (#201)
    * Subir cambio a master
    
    * Subir cambios de shipping
    
    * Subir cambios
    
    * Subir cambios comex
    
    * Subir cambios
    
    * Subir cambios a Comex
    
    * Subir cambios a master
    
    * Subir cambios
    
    * Subir cambio
    
    * subir cambio
    
    * Subir cambios KPI
    
    * Subir cambios a mtz
    
    * Subir cambios a traslados
    
    * Subiendo cambios
    
    * Subir cambios a shipping
    
    * Subiendo cambios
    
    * Subir cambios
    
    * Subir cambios del 2023
    
    * Subir cambios de redplas
    
    * Subir cambios
    
    * Subir cambios a master
    
    * Subir cambios
    
    * Subir cambios a shipping
    
    * Subir cambios a OLA
    
    * Subir cambios OLA
    
    * Subir cambios
    
    * Subir cambios a ticket
    
    * Subir cambios
    
    * subir cambios
    
    * Subir cambios modulo cartera
    
    * Subir cambios al modulo de cartera
    
    * Subir cambio modulo cartera y ticket modo response
    
    * Subir cambios al frontend
    
    * subir cambios a captura de datos

--- src/app/components/event-feria/client-igb/client-igb.component.ts
+++ src/app/components/event-feria/client-igb/client-igb.component.ts
@@ -1,158 +1,162 @@
 import { Component, OnInit } from '@angular/core';
-import { User } from '../../../models/user.model';
 import { EventService } from '../../../services/event.service';
 
 declare var $: any;
 
 @Component({
-    templateUrl: './client-igb.component.html',
-    styleUrls: ['./client-igb.component.css'],
-    providers: [EventService]
+  templateUrl: './client-igb.component.html',
+  styleUrls: ['./client-igb.component.css'],
+  providers: [EventService]
 })
 export class ClientIgbComponent implements OnInit {
-    public identity;
-    public contact: string;
-    public whsName: string;
-    public document: string;
-    public selectedRegion: string = "";
-    public city: string;
-    public phone: string;
-    public mail: string;
-    public warningMessage: string;
-    public exitMessage: string;
-    public errorMessage: string;
-    public selected: Map<number, string>;
-    public validContact: boolean = true;
-    public validPhone: boolean = true;
-    public validDocument: boolean = true;
-    public validMail: boolean = true;
-    public validSelectRegion: boolean = true;
-    public authorizeData: boolean = false;
+  public identity;
+  public contact: string;
+  public whsName: string;
+  public document: string;
+  public selectedRegion: string = "";
+  public selectedAsesor: string = "";
+  public city: string;
+  public phone: string;
+  public mail: string;
+  public warningMessage: string;
+  public exitMessage: string;
+  public errorMessage: string;
+  public selected: Map<number, string>;
+  public validContact: boolean = true;
+  public validPhone: boolean = true;
+  public validDocument: boolean = true;
+  public validMail: boolean = true;
+  public validSelectRegion: boolean = true;
+  public validSelectAsesor: boolean = true;
+  public authorizeData: boolean = false;
 
-    constructor(private _eventService: EventService) {
-        this.selected = new Map<number, string>();
+  constructor(private _eventService: EventService) {
+    this.selected = new Map<number, string>();
+  }
+
+  ngOnInit() { }
+
+  public selectedInteres(key: number, interes: string) {
+    this.errorMessage = "";
+    if (this.selected.has(key)) {
+      this.selected.delete(key);
+    } else {
+      this.selected.set(key, interes);
+    }
+  }
+
+  public captureClient() {
+    this.errorMessage = "";
+    this.exitMessage = "";
+    let interes = "";
+
+    $('#modal_transfer_process').modal({
+      backdrop: 'static',
+      keyboard: false,
+      show: true
+    });
+
+    if (this.contact == null || this.contact.length <= 0) {
+      this.validContact = false;
+      this.getScrollTop();
+      $('#modal_transfer_process').modal('hide');
+      return;
+    }
+    if (this.document == null || this.document.length <= 0) {
+      this.validDocument = false;
+      this.getScrollTop();
+      $('#modal_transfer_process').modal('hide');
+      return;
+    }
+    if (this.selectedRegion == null || this.selectedRegion.length <= 0) {
+      this.validSelectRegion = false;
+      this.getScrollTop();
+      $('#modal_transfer_process').modal('hide');
+      return;
+    }
+    if (this.phone == null || this.phone.length <= 0) {
+      this.validPhone = false;
+      this.getScrollTop();
+      $('#modal_transfer_process').modal('hide');
+      return;
+    }
+    if (this.mail == null || this.mail.length <= 0) {
+      this.validMail = false;
+      this.getScrollTop();
+      $('#modal_transfer_process').modal('hide');
+      return;
+    }
+    if (this.selectedAsesor == null || this.selectedAsesor.length <= 0) {
+      this.validSelectAsesor = false;
+      this.getScrollTop();
+      $('#modal_transfer_process').modal('hide');
+      return;
     }
 
-    ngOnInit() { }
+    for (let key of Array.from(this.selected.keys())) {
+      interes += this.selected.get(key) + " ";
+    }
+    if (interes.length <= 0) {
+      this.errorMessage = "Seleccione un interes.";
+      this.getScrollTop();
+      $('#modal_transfer_process').modal('hide');
+      return;
+    }
 
-    public selectedInteres(key: number, interes: string) {
-        this.errorMessage = "";
-        console.log('Selecciono el interes: ' + interes);
-        if (this.selected.has(key)) {
-            this.selected.delete(key);
+    let clientFeriaDTO = {
+      "documento": this.document,
+      "nombreCompleto": this.contact.toUpperCase(),
+      "telefono": this.phone,
+      "correo": this.mail.toUpperCase(),
+      "almacen": this.whsName,
+      "interes": interes.trim(),
+      "regional": this.selectedRegion,
+      "ciudad": this.city,
+      "companyName": "IGB",
+      "asesor": this.selectedAsesor
+    }
+
+    this._eventService.captureClient(clientFeriaDTO).subscribe(
+      response => {
+        if (response.code === 0) {
+          this.clearForm();
+          this.exitMessage = response.content;
         } else {
-            this.selected.set(key, interes);
+          this.errorMessage = response.content;
         }
-    }
+        this.getScrollTop();
+        $('#modal_transfer_process').modal('hide');
+      },
+      error => {
+        console.error('Ocurrio un error capturando los datos', error);
+        this.getScrollTop();
+        this.errorMessage = "Lo sentimos. Se produjo un error interno.";
+        $('#modal_transfer_process').modal('hide');
+      }
+    );
+  }
 
-    public captureClient() {
-        this.errorMessage = "";
-        this.exitMessage = "";
-        let interes = "";
+  public getData() {
+    this.errorMessage = "";
+    this.exitMessage = "";
+  }
 
-        $('#modal_transfer_process').modal({
-            backdrop: 'static',
-            keyboard: false,
-            show: true
-        });
+  public clearForm() {
+    this.document = "";
+    this.contact = "";
+    this.phone = "";
+    this.mail = "";
+    this.whsName = "";
+    this.selectedRegion = "";
+    this.authorizeData = false;
+    this.selected = new Map<number, string>();
+    this.errorMessage = "";
+    this.exitMessage = "";
+    this.city = "";
+  }
 
-        if (this.contact == null || this.contact.length <= 0) {
-            this.validContact = false;
-            this.getScrollTop();
-            $('#modal_transfer_process').modal('hide');
-            return;
-        }
-        if (this.document == null || this.document.length <= 0) {
-            this.validDocument = false;
-            this.getScrollTop();
-            $('#modal_transfer_process').modal('hide');
-            return;
-        }
-        if (this.selectedRegion == null || this.selectedRegion.length <= 0) {
-            this.validSelectRegion = false;
-            this.getScrollTop();
-            $('#modal_transfer_process').modal('hide');
-            return;
-        }
-        if (this.phone == null || this.phone.length <= 0) {
-            this.validPhone = false;
-            this.getScrollTop();
-            $('#modal_transfer_process').modal('hide');
-            return;
-        }
-        if (this.mail == null || this.mail.length <= 0) {
-            this.validMail = false;
-            this.getScrollTop();
-            $('#modal_transfer_process').modal('hide');
-            return;
-        }
-
-        for (let key of Array.from(this.selected.keys())) {
-            interes += this.selected.get(key) + " ";
-        }
-        if (interes.length <= 0) {
-            this.errorMessage = "Seleccione un interes.";
-            this.getScrollTop();
-            $('#modal_transfer_process').modal('hide');
-            return;
-        }
-
-        let clientFeriaDTO = {
-            "documento": this.document,
-            "nombreCompleto": this.contact.toUpperCase(),
-            "telefono": this.phone,
-            "correo": this.mail.toUpperCase(),
-            "almacen": this.whsName,
-            "interes": interes.trim(),
-            "regional": this.selectedRegion,
-            "ciudad": this.city,
-            "companyName": "IGB"
-        }
-        console.log(clientFeriaDTO);
-
-        this._eventService.captureClient(clientFeriaDTO).subscribe(
-            response => {
-                console.log(response);
-                if (response.code === 0) {
-                    this.clearForm();
-                    this.exitMessage = response.content;
-                } else {
-                    this.errorMessage = response.content;
-                }
-                this.getScrollTop();
-                $('#modal_transfer_process').modal('hide');
-            },
-            error => {
-                console.error('Ocurrio un error capturando los datos', error);
-                this.getScrollTop();
-                this.errorMessage = "Lo sentimos. Se produjo un error interno.";
-                $('#modal_transfer_process').modal('hide');
-            }
-        );
-    }
-
-    public getData() {
-        this.errorMessage = "";
-        this.exitMessage = "";
-        console.log('Autoriza el habeas data', this.authorizeData);
-    }
-
-    public clearForm() {
-        this.document = "";
-        this.contact = "";
-        this.phone = "";
-        this.mail = "";
-        this.whsName = "";
-        this.selectedRegion = "";
-        this.authorizeData = false;
-        this.selected = new Map<number, string>();
-        this.errorMessage = "";
-        this.exitMessage = "";
-        this.city = "";
-    }
-
-    public getScrollTop() {
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
-    }
+  public getScrollTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
 }
